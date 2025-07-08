b6e96dba85eeb835cfb5bfce85f8492854e307c3
zerafshanhakmani689 <zerafshan.hakmani123@gmail.com>
2025-07-07 17:17:56 -0700

    implement tab bar

--- app/src/main/java/com/example/auththree/ThirdActivity.kt
+++ app/src/main/java/com/example/auththree/ThirdActivity.kt
@@ -4,32 +4,63 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import com.google.android.material.tabs.TabLayout
+import android.widget.TableRow
 import android.widget.Toast
+import androidx.viewpager2.widget.ViewPager2
 
 class ThirdActivity : AppCompatActivity() {
+
+    private lateinit var tabLayout: TabLayout
+    private lateinit var viewPager2: ViewPager2
+    private lateinit var adopter: FragmentPageAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_third)
 
-        var buttonClock = findViewById<Button>(R.id.buttonClock)
-        var buttonExam = findViewById<Button>(R.id.buttonExam)
+        tabLayout = findViewById(R.id.tabLayout)
+        viewPager2 = findViewById(R.id.viewPager2)
 
+        adopter = FragmentPageAdapter(supportFragmentManager, lifecycle)
 
-        buttonClock.setOnClickListener {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.fragmentContainer, ClockFragment())
-                .addToBackStack(null)
-                .commit()
-        }
+        tabLayout.addTab(tabLayout.newTab().setText("First"))
+        tabLayout.addTab(tabLayout.newTab().setText("Second"))
 
-        buttonExam.setOnClickListener {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.fragmentContainer, ExamFragment())
-                .addToBackStack(null)
-                .commit()
-        }
+        viewPager2.adapter = adopter
 
+        tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                if (tab != null) {
+                    viewPager2.currentItem = tab.position
+                }
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+                // Do nothing or log
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+                // Do nothing or log
+            }
+
+        })
+
+        viewPager2.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback(){
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                tabLayout.selectTab(tabLayout.getTabAt(position))
+            }
+        })
 
     }
 }
+
+
+
+
+
+
+
 
